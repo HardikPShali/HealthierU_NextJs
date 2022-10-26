a77e83e7c46b1455274c8e5a3d5bce80e3ad8d36
hridayanp <hridayanphukan123@gmail.com>
2022-10-26 14:02:46 +0530

    installed react-bootstrap and bootstrap

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from '../components/Common/Header/Header'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -11,7 +12,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <Header />
+
       <main className={styles.main}>
+
+
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
